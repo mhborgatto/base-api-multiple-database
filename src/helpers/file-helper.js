3c22adb75e6f10f4545f270ff44ac9c84f9c59ff
mhborgatto <mhborgatto@gmail.com>
2022-06-21 16:53:12 -0300

    Remove nonused vars

--- src/helpers/file-helper.js
+++ src/helpers/file-helper.js
@@ -1,6 +1,5 @@
 'use strict'
 
-import fileType from 'file-type'
 import fs from 'fs'
 import path from 'path'
 
@@ -36,40 +35,6 @@ export async function uploadFileToS3(fileName) {
   return response
 }
 
-async function formatFile(base64) {
-  if (base64.substring(0, 4) === 'data')
-    base64 = (base64.split(';base64,'))[1]
-
-  let fileMime
-  const base64Buffer = new Buffer.from(base64, 'base64')
-  fileMime = await fileType.fromBuffer(base64Buffer)
-
-  if (!fileMime) {
-    fileMime = buildFileMime(base64)
-    console.log({ manualMime: fileMime })
-  }
-
-  return {
-    fileBuffer: base64Buffer,
-    fileMime: fileMime,
-  }
-}
-
-export async function uploadBufferFileToS3(fileName, base64) {
-  const { fileBuffer, fileMime } = await formatFile(base64)
-
-  const awsBody = {
-    bucket: 'PandoraSales',
-    fileBuffer,
-    fileMime,
-    fileName
-  }
-
-  const response = await aws.uploadBufferFileToS3(awsBody)
-  return response
-}
-
-
 export function listDirFiles(dirPath, arrayOfFiles) {
   const files = fs.readdirSync(dirPath)
 
@@ -84,14 +49,4 @@ export function listDirFiles(dirPath, arrayOfFiles) {
   })
 
   return arrayOfFiles
-}
-
-function buildFileMime(base64) {
-  switch (base64.charAt(0)) {
-  case FileTypes.PDF.char:
-    return FileTypes.PDF.fullMime
-
-  case FileTypes.XML.char:
-    return FileTypes.XML.fullMime
-  }
 }
